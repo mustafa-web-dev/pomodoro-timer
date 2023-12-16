af1241fbd1cad34a3af2190570853ef67d3605d7
mustafa-web-dev <mustafacoding2022@gmail.com>
2023-12-16 21:39:41 +0200

    Initiating Project

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="h-screen bg-gray-500 flex justify-center items-center">
+    <div class="grid grid-flow-col gap-5 text-center auto-cols-max">
+      <div class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+        <span class="countdown font-mono text-5xl">
+          <span style="--value:15;"></span>
+        </span>
+        days
+      </div>
+      <div class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+        <span class="countdown font-mono text-5xl">
+          <span style="--value:10;"></span>
+        </span>
+        hours
+      </div>
+      <div class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+        <span class="countdown font-mono text-5xl">
+          <span style="--value:24;"></span>
+        </span>
+        min
+      </div>
+      <div class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+        <span class="countdown font-mono text-5xl">
+          <span style="--value:54;"></span>
+        </span>
+        sec
+      </div>
+    </div>
+  </div>
+</template>
+  
+<script setup lang="ts"></script>
