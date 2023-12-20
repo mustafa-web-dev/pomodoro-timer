23dc102062c911395041355f192a9d7fb53e3be2
mustafa-web-dev <mustafacoding2022@gmail.com>
2023-12-20 19:47:14 +0200

    Vercel analytics

--- pages/index.vue
+++ pages/index.vue
@@ -46,9 +46,6 @@
   
 <script setup>
 import { CgCoffee, BsPauseFill, BsPlayFill, PhBrain, CaReset } from "@kalimahapps/vue-icons";
-import { inject } from '@vercel/analytics';
- 
-inject();
 
 const indicator = ref('Focus')
 const started = ref(false)
